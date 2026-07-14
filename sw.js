4b5cb3657f35a3a48bbfbf2da297747a6909f816
nvthahv03-math <nvthahv03@gmail.com>
2026-07-14 11:47:57 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'ttdhs-hva-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './site.webmanifest',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png',
+  './favicon.ico'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key!== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
