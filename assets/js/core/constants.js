8320eb9347155fec2d08af3046c304a5252f6a43
nvthahv03-math <nvthahv03@gmail.com>
2026-07-27 23:46:17 +0700

    Create constants.js

---
+++ assets/js/core/constants.js
@@ -0,0 +1,23 @@
+export const APP_NAME = 'TTDHS_HVA';
+export const APP_TITLE = 'Trung tâm Điều hành số THPT Hòa Vang';
+
+export const BRAND = {
+    deep: '#F5F7FA',
+    primary: '#FFFFFF',
+    accent: '#E2E8F0',
+    teal: '#0F4C81',
+    glow: '#14B8A6'
+};
+
+export const STORAGE_KEYS = {
+    USER: 'user',
+    PWA_INSTALLED: 'pwa_installed',
+    HIDE_IOS_GUIDE: 'hideIOSGuide'
+};
+
+export const MODULES = {
+    HOITHAO: 'modules/hoithao.html',
+    // Thêm module mới tại đây
+};
+
+export const DAYS_VN = ['Chủ nhật', 'Thứ Hai', 'Thứ Ba', 'Thứ Tư', 'Thứ Năm', 'Thứ Sáu', 'Thứ Bảy'];
