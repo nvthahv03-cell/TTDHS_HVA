3ab36c4ecbf05a92ae1407a1ab28d6124b21992e
nvthahv03-math <nvthahv03@gmail.com>
2026-09-06 06:24:27 +0700

    Update home-renderer.js

--- assets/js/features/home-renderer.js
+++ assets/js/features/home-renderer.js
@@ -2312,21 +2312,21 @@ export function renderHome() {
 </a>
 
 
-                    <button type="button"
-                            class="w-full flex items-center gap-2.5
-                                   px-3 py-2 rounded-lg
-                                   hover:bg-indigo-50
-                                   dark:hover:bg-slate-800 transition text-left">
+                   <a href="KhaosatBinhchon.html"
+   class="w-full flex items-center gap-2.5
+          px-3 py-2 rounded-lg
+          hover:bg-indigo-50
+          dark:hover:bg-slate-800 transition text-left">
 
-                        <i class="bi bi-ui-checks-grid
-                                  text-indigo-600 w-5 text-center"></i>
+    <i class="bi bi-ui-checks-grid
+              text-indigo-600 w-5 text-center"></i>
 
-                        <span class="text-[11px] font-semibold
-                                     text-slate-700 dark:text-slate-200">
-                            Khảo sát - Bình chọn
-                        </span>
+    <span class="text-[11px] font-semibold
+                 text-slate-700 dark:text-slate-200">
+        Khảo sát - Bình chọn
+    </span>
 
-                    </button>
+</a>
 
 
                     <button type="button"
